97634d6913e72b4b1480519424f5561a3a9e609f
1072009 <46085539+1072009@users.noreply.github.com>
2018-12-24 19:27:00 +0800

    1072009
    增加#define angle 60
    增加註解

--- checker.cpp
+++ checker.cpp
@@ -5,30 +5,30 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
+#define angle 60
 using namespace std;
 const int N=17;
-int A[N][N];
+int A[N][N];///宣告二維陣列
 void readboard(int A[N][N]);
 void showBoard();
 void go();
 void main()
 {
-	//1052003
-	readboard(A);
-	showBoard();
+	readboard(A);///讀取棋盤
+	showBoard();///顯示棋盤
 	go();
 }
 void readboard(int A[N][N])
 {
 	ifstream file;
-	file.open("board.txt");
+	file.open("board.txt");///打開board文字檔
 	int i,j;
 	for(i=0;i<N;i++)
 		for(j=0;j<N;j++)
 		{
 			file>>A[i][j];
 		}
-		file.close();
+		file.close();///關閉board文字檔
 }
 void go()
 {
@@ -42,8 +42,8 @@ void showBoard()
 	ShowWindow(hwnd,SW_SHOW);
 	HDC hdc=GetDC(hwnd);
 	double l=50;
-	double dx=l*cos(60*3.14159/180.);
-	double dy=l*sin(60*3.14159/180.);
+	double dx=l*cos(angle*3.14159/180.);
+	double dy=l*sin(angle*3.14159/180.);
 	int Ox=100;
 	int Oy=100;
 	int r=20;
@@ -54,6 +54,6 @@ void showBoard()
 			int x=Ox+(2*i+j)*dx;
 			int y=Oy+j*dy;
 			if(A[i][j]>0)
-				Ellipse(hdc,x-r,y-r,x+r,y+r);
+				Ellipse(hdc,x-r,y-r,x+r,y+r);///棋子走的方式
 		}
 }
